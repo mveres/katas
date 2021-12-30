ca7fdc25fcee2b66fd99931f9d7ec88871f08639
mveres <veres85@yahoo.com>
2021-12-30 16:03:33 +0200

    tests and solution

--- index.test.ts
+++ index.test.ts
@@ -1,5 +1,35 @@
-import { sum } from './index';
+import { sums, sumsFaster } from './index';
 
-test('sum(1,2) should be 3', () => {
-	expect(sum(1,2)).toBe(3);
+test('sums([], [])) is []', () => {
+  expect(sums([], [])).toEqual([]);
+  expect(sumsFaster([], [])).toEqual([]);
+});
+
+test('sums([1, 2], [[0,1]]) is [3]', () => {
+  expect(sums([1, 2], [[0, 1]])).toEqual([3]);
+  expect(sumsFaster([1, 2], [[0, 1]])).toEqual([3]);
+});
+
+test('sums([-2, 0, 3, -5, 2, -1], [[0, 2], [2, 5], [0, 5]]*)  is  [1, -1, -3]', () => {
+  expect(
+    sums(
+      [-2, 0, 3, -5, 2, -1],
+      [
+        [0, 2],
+        [2, 5],
+        [0, 5],
+      ]
+    )
+  ).toEqual([1, -1, -3]);
+
+  expect(
+    sumsFaster(
+      [-2, 0, 3, -5, 2, -1],
+      [
+        [0, 2],
+        [2, 5],
+        [0, 5],
+      ]
+    )
+  ).toEqual([1, -1, -3]);
 });
--- index.ts
+++ index.ts
@@ -1,1 +1,23 @@
-export const sum = (a: number, b: number) => a + b;
+// O(n^2)
+export const sums = (
+  transactions: number[],
+  intervals: [number, number][]
+): number[] =>
+  intervals.map(([left, right]) =>
+    transactions.slice(left, right + 1).reduce((a, b) => a + b)
+  );
+
+// O(n)
+export const sumsFaster = (
+  transactions: number[],
+  intervals: [number, number][]
+): number[] => {
+  const allSums = transactions.reduce(
+    (sums, t, i) => [...sums, t + (sums[i - 1] || 0)],
+    []
+  );
+
+  return intervals.map(
+    ([left, right]) => allSums[right] - (allSums[left - 1] || 0)
+  );
+};
